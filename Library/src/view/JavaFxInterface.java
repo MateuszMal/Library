cedb68587fb3c1e4d79b24f1f3ee69221d360b31
230191 Malenta Mateusz <230191@edu.p.lodz.pl>
2020-12-20 23:34:45 +0100

    Utworzenie okan glownego aplikacji

--- Library/src/view/JavaFxInterface.java
+++ Library/src/view/JavaFxInterface.java
@@ -1,24 +1,37 @@
 package view;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class JavaFxInterface extends Application {
+	
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(this.getClass().getResource("./fxml/StackPaneWindow.fxml"));
-		StackPane stackPane = loader.load();
+		BorderPane borderPane = loader.load();
 		
-		Scene scene = new Scene(stackPane);
+		LibraryViewController controller = loader.getController();
+		
+		Scene scene = new Scene(borderPane);
 		
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Library");
 		primaryStage.show();
 		}
+	
+//	@FXML
+//	private void loadBookBorderPane(ActionEvent event) throws Exception{
+//		BorderPane pane = FXMLLoader.load(getClass().getResource("./fxml/BookBorderPane.fxml"));
+//		borderPane.getChildren().setAll(pane);
+//	}
 }
