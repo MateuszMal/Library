572b91faf3c97e855f956d65fbac99fd9afc8c51
230191 Malenta Mateusz <230191@edu.p.lodz.pl>
2020-12-26 15:21:27 +0100

    W RentController checkBox zeruje pola wpisu

--- Library/src/view/RentController.java
+++ Library/src/view/RentController.java
@@ -7,6 +7,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import utils.FxmlUtils;
 
@@ -14,16 +16,16 @@ public class RentController implements Initializable {
 
 	private FxmlUtils fxmlUtils = new FxmlUtils();
 	
+	
 	private InterfaceController interfaceController;
-
-
 	@FXML
 	private BorderPane rentBorderPane;
-
 	@FXML
-	private Button rentBookButton;
+	private TextField rentTextField;	
 	@FXML
-	private Button returnButton;
+	private TextField returnTextField;
+	@FXML
+	private CheckBox allRentCheckBox;
 	@FXML
 	private Button rentBackButton;
 
@@ -38,4 +40,11 @@ public class RentController implements Initializable {
 		fxmlUtils.fxmlLoader(event, "../view/fxml/StackPaneWindow.fxml", rentBorderPane);
 	}
 
+	public void onReturnTextField(ActionEvent event) {
+		if(allRentCheckBox.isSelected()) {
+			rentTextField.clear();
+			returnTextField.clear();
+			// TODO Dodac wyswietlanie wszystkich wypozyczen
+		}
+	}
 }
