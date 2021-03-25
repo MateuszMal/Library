103311f0784ffd0dc537b7e65983c4a1530773f1
230191 Malenta Mateusz <230191@edu.p.lodz.pl>
2021-03-25 23:01:20 +0100

    Zmiana dziedziczenia w Tabloid

--- Library/src/controller/Tabloid.java
+++ Library/src/controller/Tabloid.java
@@ -1,5 +1,39 @@
 package controller;
 
-public class Tabloid {
+import java.util.ArrayList;
+
+public class Tabloid extends Newspaper {
+	
+	
+
+	public Tabloid(String title, Author author, int numberOfPages) {
+		super(title, author, numberOfPages);
+		// TODO Auto-generated constructor stub
+	}
+
+	private String advertisement;
+	private ArrayList<String> advertisementList;
+	
+
+	
+	public void addAdvertisement(String advertString) {
+		advertisementList.add(advertString);
+	}
+
+	public String getAdvertisement() {
+		return advertisement;
+	}
+
+	public void setAdvertisement(String advertisement) {
+		this.advertisement = advertisement;
+	}
+
+	public ArrayList<String> getAdvertisementList() {
+		return advertisementList;
+	}
+
+	public void removeFromList(int n) {
+		advertisementList.remove(n);
+	}
 
 }
